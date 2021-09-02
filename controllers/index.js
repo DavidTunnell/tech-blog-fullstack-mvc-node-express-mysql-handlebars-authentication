7fb0a7522cdcacd05724a0d370d7207b9999ff30
David Tunnell <davidtunnell9@gmail.com>
2021-09-02 15:36:06 -0500

    project client being rendered now has css and javascript that renders strings from db on client

--- controllers/index.js
+++ controllers/index.js
@@ -8,14 +8,4 @@ router.use('/', homeRoutes);
 router.use('/test', testRoutes);
 router.use('/api', apiRoutes);
 
-// router.get('/', async(req, res) => {
-//     // find all categories and associated products
-//     try {
-//         const testData = await Test.findAll();
-//         res.status(200).json(testData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
 module.exports = router;
