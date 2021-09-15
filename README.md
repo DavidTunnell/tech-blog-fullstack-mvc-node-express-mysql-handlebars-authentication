e70a34c38ef8c3958129a162fdce62120649c8f5
David Tunnell <davidtunnell9@gmail.com>
2021-09-15 10:16:59 -0500

    added jawsdb connection for heroku

--- README.md
+++ README.md
@@ -97,12 +97,8 @@ POST - http://localhost:3001/api/post/ - `{ "title": "Title of a Blog Post", "co
 
 # ![logout screenshot](./assets/logout.png)
 
-//eslint validation
-
 //deployed to heroku - url to application
 
 // Application loads with no errors. - w2 nu checker
 
 //- YT vid for understanding cookies and sessions especially related to express if possible
-
-//reverse order of comments on page with sequelize joins/order syntax correctly
--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
+let sequelize;
 //sequelize object with .env config parameter used throughout app
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306,
-    }
-);
+//if statement contains logic to run on heroku or local
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306,
+        }
+    );
+}
 
 module.exports = sequelize;
